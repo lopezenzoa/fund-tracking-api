b4959fecaf5c0ec81e59bc26056e7ffba6d5a8f5
lopezenzoa <enzoagustinlopez2003@gmail.com>
2026-08-14 11:36:55 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/fund_tracking_api/FundTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.fund_tracking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundTrackingApiApplication.class, args);
+	}
+
+}
